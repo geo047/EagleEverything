4d35837c77442b39be6dbf2d58b2929cf9c65378
Andrew George <andrew.george@csiro.au>
2019-06-25 12:19:45 +1000

    Working version of QR where R matrix passed into code, analysed, and passed back to R session

--- MyPackage/GPUtestCode/RcppExample.cpp
+++ MyPackage/GPUtestCode/RcppExample.cpp
@@ -3,50 +3,29 @@
 #include<magma_lapack.h>
 #include<magma_operators.h>
 
-
-#ifdef _OPENMP
-#include <omp.h>
-//   [[Rcpp::plugins(openmp)]]
-#endif
-
-
-
 using namespace Rcpp;
 
 
 
 
 // [[Rcpp::export]]
-//RcppExport SEXP gpuQR_magma(SEXP X)
-//Rcpp::NumericMatrix   gpuQR_magma(Rcpp::NumericMatrix X)
-RcppExport SEXP   gpuQR_magma(SEXP X_)
-
+Rcpp::NumericVector   gpuQR_magma(Rcpp::NumericMatrix X)
 {
- NumericMatrix X(X_);
-   Rcpp::Rcout << X[0,0] << std::endl;
-   Rcpp::Rcout << X[0,1] << std::endl;
-   Rcpp::Rcout << X[0,2] << std::endl;
-   Rcpp::Rcout << X[0,3] << std::endl;
+
+   // convert to C++ type
+   double const* d_X = X.begin();
 
     // Initialize magma and cublas
     magma_init();
- //   cublasInit();
 
    magma_print_environment();
 
    magma_queue_t queue = NULL ;
    magma_int_t dev =0;
    magma_queue_create (dev ,& queue );
-    const double             d_neg_one = MAGMA_D_NEG_ONE;
-    const double             d_one     = MAGMA_D_ONE;
-    const double c_neg_one = MAGMA_D_NEG_ONE;
-    const double c_one     = MAGMA_D_ONE;
     const double c_zero    = MAGMA_D_ZERO;
-    const magma_int_t        ione      = 1;
    
-    real_Double_t    gflops, gpu_perf, gpu_time, cpu_perf=0, cpu_time=0;
-    double           Anorm, error=0, error2=0;
-    double *h_A, *h_R, *tau, *h_work, tmp[1], unused[1];
+    double  *tau,  tmp[1], unused[1];
     magmaDouble_ptr d_A, dT;
     magma_int_t M, N, n2, lda, ldda, lwork, info, min_mn, nb, size;
    
@@ -64,10 +43,6 @@ RcppExport SEXP   gpuQR_magma(SEXP X_)
     lwork = (magma_int_t)MAGMA_D_REAL( tmp[0] );
 
     magma_dmalloc_cpu( &tau,    min_mn );
-    magma_dmalloc_cpu( &h_A,    n2     );
-    magma_dmalloc_cpu( &h_work, lwork  );
-
-    magma_dmalloc_pinned( &h_R,    n2     );
 
     magma_dmalloc( &d_A,    ldda*N );
 
@@ -75,22 +50,6 @@ RcppExport SEXP   gpuQR_magma(SEXP X_)
     magma_dmalloc( &dT, size );
     magmablas_dlaset( MagmaFull, size, 1, c_zero, c_zero, dT, size, queue );
  
-   /* Initialize the matrix */
-  // hard to get this to work ... 
-//   magma_generate_matrix( opts, M, N, h_A, lda );
-
-// Randomize the matrix  Alternate mehtod for generating a matrix
-magma_int_t ISEED [4] = {0 ,0 ,0 ,1}; // seed
-lapackf77_dlarnv ( &ione , ISEED , &n2 , h_A ); // randomize a
-// print matrix contents on host device
-std::cout << "THIS HAS WORKED - Generated matrix via lapack thingie " << std::endl;
-magma_dprint(M,N, h_A, lda);
-
-
-
-
-
-
  /* ====================================================================
                Performs operation using MAGMA
     =================================================================== */
@@ -99,33 +58,26 @@ magma_dprint(M,N, h_A, lda);
 // MAGMA
  double *d_a ; // d_a mxn matrix on the device
 magma_dmalloc (&d_a , ldda*M); // device memory for d_a
-magma_dsetmatrix ( M, N, h_A ,M,d_a ,ldda , queue ); // copy a -> d_a
-// see if matrix on gpu matches matrix on host
-std::cout << "THIS HAS WORKED - see if matrix on gpu matches matrix on host " << std::endl;
-magma_dprint_gpu(M,N, d_a, ldda, queue);
 
-
-
+magma_dsetmatrix ( M, N, d_X ,M,d_a ,ldda , queue ); // copy X -> d_a
+std::cout << " This is what is now sitting in d_a on the GPU ready for analysis" << std::endl;
+magma_dprint_gpu(M, N, d_a , ldda, queue);   // print contents of matrix on gpu device
 
 
 
 // QR algorithm
 magma_dgeqrf2_gpu( M, N, d_a, ldda, tau, &info );
-std::cout << "Results coming out of dgeqrf2 " << std::endl;
- magma_dprint_gpu(M, N, d_a , ldda, queue);   // print contents of matrix on gpu device
+std::cout << "Results coming out of dgeqrf2 - has d_a changed? " << std::endl;
+magma_dprint_gpu(M, N, d_a , ldda, queue);   // print contents of matrix on gpu device
 
- double *r;
-  magma_dmalloc_pinned (&r,n2 ); // host memory for r
- magma_dgetmatrix ( M, N, d_a ,ldda ,r, M , queue ); // copy d_a -> r
-std::cout << "printing out contents of r " << std::endl;
+double *r;
+magma_dmalloc_pinned (&r,n2 ); // host memory for r
+magma_dgetmatrix ( M, N, d_a ,ldda ,r, M , queue ); // copy d_a -> r
+std::cout << "Contents of r after moving contents d_a to r which is in host memory " << std::endl;
 magma_dprint(M,N, r, lda);
   
 
 
-std::cout << "About to start transfer or r into R land ... " << std::endl;
-std::cout << n2 << std::endl;
-std::cout << sizeof(r) << std::endl;
-std::cout << sizeof(*r) << std::endl;
 
 NumericVector ans =  NumericVector(r,r + n2  );
 ans.attr("dim") = Dimension(M, N);
