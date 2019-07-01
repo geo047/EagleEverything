aee4550435b6cde5f530733dfd6ea1bcb3c73d7e
Andrew George <andrew.george@csiro.au>
2019-07-02 09:21:10 +1000

    02/07/19 morning commit - about to clean dir, scripts, and code

--- MyPackage/GPUtestCode/RcppExampleEigen.cpp
+++ MyPackage/GPUtestCode/RcppExampleEigen.cpp
@@ -10,47 +10,44 @@
 
 using namespace Rcpp;
 
-// Potential issue
-// First, you are accessing the matrix row-wise, whereas MAGMA and LAPACK use column-wise ordering. That is,
-//
-// A[ i + j*size ]
-
 
 // [[Rcpp::export]]
-Rcpp::NumericVector   gpuEigen_magma(const Rcpp::NumericMatrix&  X)
+Rcpp::List   gpuEigen_magma(const Rcpp::NumericMatrix&  X)
 {
 
-
-magma_init (); // initialize Magma
+// Initialize Magma and print GPU environment
+magma_init (); 
 magma_print_environment();
 
 
-
-
+// Initialize the queue
 magma_queue_t queue = NULL ;
 magma_int_t dev =0;
 magma_queue_create (dev ,& queue );
+
+
+// Variables 
 double gpu_time , cpu_time ;
 magma_int_t n=X.nrow() , n2=n*n;
 magma_int_t  ldda = magma_roundup( n, 32 );
 
 
-double *a, *r; // a, r - nxn matrices on the host
+double *r; //  r - nxn matrices on the host
+double *h_r ;   // nxn matrix with eigenvector on host
 double *d_r ; // nxn matrix on the device
 double * h_work ; // workspace
 magma_int_t lwork ; // h_work size
 magma_int_t * iwork ; // workspace
 magma_int_t liwork ; // iwork size
-double *w1 , *w2; // w1 ,w2 - vectors of eigenvalues
+double *w1  ; // w1  - vectors of eigenvalues
 double error , work [1]; // used in difference computations
 magma_int_t ione = 1, info ;
 double mione = -1.0;
 magma_int_t incr = 1;
 magma_int_t ISEED [4] = {0 ,0 ,0 ,1}; // seed
 magma_dmalloc_cpu (&w1 ,n); // host memory for real
-magma_dmalloc_cpu (&w2 ,n); // eigenvalues
-magma_dmalloc_cpu (&a,n2 ); // host memory for a
 magma_dmalloc_cpu (&r,n2 ); // host memory for r
+magma_dmalloc_cpu (&h_r,n2 ); // host memory for r
 magma_dmalloc (& d_r ,n2 ); // device memory for d_r
 //----> magma_dmalloc (& d_r ,ldda * n ); // device memory for d_r
 
@@ -70,91 +67,50 @@ magma_dmalloc_cpu (& h_work , lwork ); // memory for workspace
 
 // Convert X into a form that Magma can deal with of type double *
 // Have to do it in stages
-// std::vector <double> xx = Rcpp::as<std::vector<double> >(X);
-double const *xx = &X(0,0);
-// ----> double xxx[xx.size()];
-// ----> for(int i=0;i<xx.size();i++)
-// ---->    xxx[i] = xx[i];
+// double const *xx = &X(0,0);
+double const *xx = X.begin();
 
-  
-// ----> magma_dsetmatrix ( n, n, xxx  , n, d_r ,n , queue ); // copy a -> d_r
-magma_dsetmatrix ( n, n, xx  , n, d_r ,n , queue ); // copy a -> d_r
-std::cout << "contents of r " << std::endl;
-magma_dprint(5,5, r, n );
-std::cout << "contents of d_r " << std::endl;
+// Copy contents of X that sit in CPU memory to GPU memory : xx -> d_r  
+magma_dsetmatrix ( n, n, xx  , n, d_r ,n , queue ); 
+std::cout << "Contents of d_r " << std::endl;
 magma_dprint_gpu(5,5, d_r, n , queue);
 
-
-// copy X to host memory 
-double *h_TEST;
-std::cout << "Testing copy idea  allocating h_TEST with dmalloc_cpu " << std::endl;
-magma_dmalloc_cpu (&h_TEST,n2 ); // host memory for a
-std::cout << "Testing copy idea " << std::endl;
-
-//for(int i=0; i<n; i++){
-//  for(int j=0; j<n; j++){
-//     h_TEST[i+j*n] <- X[i,j];
-//     std::cout << X[i,j] << std::endl;
-//  }
-//}
- magma_int_t lda = n;
- magma_int_t ldq = n;
-std::cout << "Copying X to h_TEST " << std::endl;
- lapackf77_dlacpy( "Full", &n,   &n, &X[0], &lda, &h_TEST[0], &ldq );
-std::cout << "Print h_TEST " << std::endl;
-// // --->  magma_dprint(n,n,h_TEST,n);
-
-std::cout << "Print X " << std::endl;
-double const* XX = X.begin();
-// --->  magma_dprint(n,n,XX ,n);
-
-
-
-std::cout << " 1 " << std::endl;
-// -----> magma_dsetmatrix ( n, n, h_TEST , n, d_r ,n, queue ); // copy a -> d_r
-std::cout << " 2 " << std::endl;
-
-
-
-// compute the eigenvalues and eigenvectors for a symmetric ,
-// real nxn matrix ; Magma version
-//magma_dsyevd_gpu(MagmaVec,MagmaLower,n,d_r,n,w1,r,n,h_work,
-//lwork,iwork,liwork,&info);
-
-
-
-
-
-//magma_dsetmatrix ( n, n, h_X, n, d_r ,n, queue ); // copy a -> d_r
+// Doing eigenvalue calculation in GPU land
  magma_dsyevd_gpu(MagmaVec,MagmaLower,n,d_r,n,w1,r,n,h_work,
  lwork,iwork,liwork,&info);
 
-std::cout << "contents of r " << std::endl;
+std::cout << "EigenVectors on the GPU  " << std::endl;
 magma_dprint_gpu(5,5,d_r,n, queue);
 
 
-std::cout << "Wahoo !!!! " <<std::endl;
 
 
-NumericVector ans =  NumericVector(w1,w1 + n  );
-// ans.attr("dim") = Dimension(M, N);
+magma_dgetmatrix(n,n,d_r, n, h_r, n, queue);
+std::cout << "EigenVectors on the CPU  " << std::endl;
+magma_dprint(5,5, h_r,n );
+std::cout << " -----------------------------  " << std::endl;
 
 
+
+NumericVector vectors = NumericVector(h_r, h_r + n2);
+std::cout << " in here " << std::endl;
+vectors.attr("dim") = Dimension(n,n);
+
+std::cout << " in here " << std::endl;
+// Return list structure 
+return Rcpp::List::create( 
+       Rcpp::Named("values") =   NumericVector(w1,w1 + n  ) , 
+       Rcpp::Named("vectors") =  vectors );
+
+std::cout << " in here " << std::endl;
 
 magma_free_cpu (w1 ); // free host memory
-magma_free_cpu (w2 ); // free host memory
-magma_free_cpu (a); // free host memory
 magma_free_cpu (r); // free host memory
 magma_free_cpu ( h_work ); // free host memory
-magma_free_cpu (h_TEST); // free host memory
+magma_free_cpu ( h_r ); // free host memory
 magma_free (d_r ); // free device memory
 magma_queue_destroy ( queue ); // destroy queue
 magma_finalize (); // finalize Magma
-
-
-
-return ans ;
-
 
 
 
