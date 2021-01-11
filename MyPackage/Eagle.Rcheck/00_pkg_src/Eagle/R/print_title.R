d2bad767657478854aa00022f3292f21020e0a91
Andrew George <andrew.george@csiro.au>
2021-01-11 19:22:55 +1100

    Version 2.4.3 final

---
+++ MyPackage/Eagle.Rcheck/00_pkg_src/Eagle/R/print_title.R
@@ -0,0 +1,18 @@
+
+.print_title <- function(){
+    ## internal function: use only in AM function
+    ## title
+    message("\n\n\n")
+message("                    Multiple-Locus Association Mapping")
+message("                            Version 2.4.3 \n")
+message(" ")
+message("   . ,-\"-.   ,-\"-. ,-\"-.   ,-\"-. ,-\"-. ,-\"-. ,-\"-.   ,-\"-. ,-\"-.    ")
+message("    X | | \\ / | | X | | \\ / | | X | | \\ / | | X | | \\ / | | X | | \\ /   ")
+message("   / \\| | |X| | |/ \\| | |X| | |/ \\| | |X| | |/ \\| | |X| | |/ \\| | |X|   ")
+message("      `-!-' `-!-\"   `-!-' `-!-'   `-!-' `-!-\"   `-!-' `-!-'   `-!-' `-     \n\n")
+
+}
+
+
+
+
